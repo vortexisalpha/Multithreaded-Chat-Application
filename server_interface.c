3ef06c85e71014f3fde8844cd8b77ba8290109c1
vortexisalpha <joshuaboshua250505@gmail.com>
2025-11-20 15:14:52 +0000

    server interface foundation complete

--- server_interface.c
+++ server_interface.c
@@ -1,26 +1,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "udp.h"
-#include "queue.h"
+#include "server_types.h"
 
-typedef struct {
-    client_node_t* next;
-    struct sockaddr_in client_address;
-    char client_name[NAME_SIZE];
-} client_node_t;
+// make this void *
 
-void add_client_to_list(client_node_t * tail, struct sockaddr_in *addr, char name[NAME_SIZE]){
-    client_node_t * new_node;
-    strcpy(new_node->client_name, name);
-    new_node->client_address = *addr;
-    tail->next = new_node;
-    tail = tail->next;
-}
-
-void *listener(void *queue, int * sd, Queue * task_queue){
-    //takes in the queue and adds the command into it in the form:
-    //[]
+void *listener(void *args){
+    listner_args_t* listner_args = (listner_args_t*)args;
+    //takes in the queue and adds the command into it,
+    //queue is of form: [queue_node_t,...] each queue_node_t has msg and from addr
     while (1) 
     {
         // Storage for request and response messages
@@ -32,38 +20,24 @@ void *listener(void *queue, int * sd, Queue * task_queue){
         // Variable to store incoming client's IP address and port
         struct sockaddr_in client_address;
     
-        int rc = udp_socket_read(&sd, &client_address, client_request, BUFFER_SIZE);
+        int rc = udp_socket_read(&listner_args->sd, &client_address, client_request, BUFFER_SIZE);
 
         if (rc > 0){
-            q_append(task_queue, client_request, client_address); // this includes queue full sleep for thread
+            q_append(listner_args->task_queue, client_request, client_address); // this includes queue full sleep for thread
         }
     }
 }
 
-typedef struct {
-    int sd;
-    command_t* command;
-    struct sockaddr_in* from_addr;
-    client_node_t *head;
-    client_node_t *tail;
-} execute_command_args_t;
 
-setup_command_args(execute_command_args_t* args, int sd, command_t* command, struct sockaddr_in* from_addr,client_node_t *head, client_node_t *tail){
-    args->sd = sd;
-    args->command = command;
-    args->from_addr = from_addr;
-    args->head = head;
-    args->tail = tail;
-}
-
-void *connect_to_server(void* arg){
-    execute_command_args_t* cmd_args = (execute_command_args_t*)arg; // cast to input type struct
+void *connect_to_server(void* args){
+    execute_command_args_t* cmd_args = (execute_command_args_t*)args; // cast to input type struct
     char* name = cmd_args->command->args[0];
     add_client_to_list(cmd_args->tail, cmd_args->from_addr, name);
 
     char server_response[BUFFER_SIZE];
     sprintf(server_response, "CONNECTED %s", name);
     int rc = udp_socket_write(cmd_args->sd, &cmd_args->from_addr, server_response, BUFFER_SIZE);
+    printf("Request served...\n");
 }
 
 
@@ -80,7 +54,7 @@ void spawn_execute_command_threads(int sd, command_t* command, struct sockaddr_i
     }
 }
 
-void *queue_thread(Queue * task_queue, int sd, client_node_t *head, client_node_t *tail){
+void *queue_manager(Queue * task_queue, int sd, client_node_t *head, client_node_t *tail){
     while(1){
         char * tokenised_command[MAX_COMMAND_LEN];
         struct sockaddr_in from_addr;
@@ -116,36 +90,15 @@ int main(int argc, char *argv[])
     Queue task_queue;
     setup(sd, &task_queue);
     
-    // Server main loop
-    while (1) 
-    {
-        // Storage for request and response messages
-        char client_request[BUFFER_SIZE], server_response[BUFFER_SIZE];
+    //spawn listner
+    pthread_t listener_thread;
+    listener_args_t *listener_args;
+    setup_listener_args(listener_args, sd, &task_queue);
+    pthread_create(&listener_thread, NULL, listener, listener_args);
+    //pthread_join(t, NULL); //?
 
-        // Demo code (remove later)
-        printf("Server is listening on port %d\n", SERVER_PORT);
+    //spawn queue thread
 
-        // Variable to store incoming client's IP address and port
-        struct sockaddr_in client_address;
-    
-        int rc = udp_socket_read(sd, &client_address, client_request, BUFFER_SIZE);
-
-        // Successfully received an incoming request
-        if (rc > 0)
-        {
-            // Demo code (remove later)
-            strcpy(server_response, "Hi, the server has received: ");
-            strcat(server_response, client_request);
-            strcat(server_response, "\n");
-            char* port_n;
-            sprintf(port_n, "client port: %d", ntohs(client_address.sin_port));
-            strcat(server_response, port_n);
-
-            rc = udp_socket_write(sd, &client_address, server_response, BUFFER_SIZE);
-
-            printf("Request served...\n");
-        }
-    }
 
     return 0;
 }
