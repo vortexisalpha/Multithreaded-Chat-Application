611e46208ef0b9a7da13b8dbe3cc463dfc26b21d
vortexisalpha <joshuaboshua250505@gmail.com>
2025-11-20 13:14:45 +0000

    queue full cond and changed queue data to queue_node_t to store sender addr aswell

--- queue.h
+++ queue.h
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
+#include <netinet/in.h>
 
 #define QUEUE_MAX 256
 #define QUEUE_MSG_SIZE 512
@@ -8,12 +9,17 @@
 
 
 typedef struct {
-    char data[QUEUE_MAX][QUEUE_MSG_SIZE];
+    char msg[QUEUE_MSG_SIZE];
+    struct sockaddr_in from_addr;
+} queue_node_t;
+typedef struct {
+    queue_node_t * data[QUEUE_MAX];
     int head;
     int tail;
     int size;
     pthread_mutex_t lock;
     pthread_cond_t nonempty;
+    pthread_cond_t nonfull;
 } Queue;
 
 void queue_init(Queue *q){
@@ -21,11 +27,13 @@ void queue_init(Queue *q){
     q->tail = 0;
     pthread_mutex_init(&q->lock, NULL);
     pthread_cond_init(&q->nonempty, NULL);
+    pthread_cond_init(&q->nonfull, NULL);
+
 }
 
 void get_and_tokenise(Queue * q, int idx, char * args[]){
 
-    char *token = strtok(q->data[idx], " "); 
+    char *token = strtok(q->data[idx]->msg, " "); 
     int argsc = 0; 
     while (token != NULL && argsc < MAX_COMMAND_LEN) // max len command = sayto, from, to, msg = len 4
     {
@@ -38,10 +46,16 @@ void get_and_tokenise(Queue * q, int idx, char * args[]){
 }
 
 //add to the thread and give nonempty signal under mutex
-void q_append(Queue* q, char* msg){
+void q_append(Queue* q, char* msg, struct sockaddr_in from_addr){
     pthread_mutex_lock(&q->lock);
+    while ((q->tail + 1) % QUEUE_MAX == q->head) { // queue full
+        pthread_cond_wait(&q->nonfull, &q->lock);
+    }   
     q->size++;
-    strncpy(q->data[q->tail], msg, QUEUE_MSG_SIZE);
+
+    strncpy(q->data[q->tail]->msg, msg, QUEUE_MSG_SIZE);
+    q->data[q->tail]->from_addr = from_addr;
+
     q->tail = (q->tail + 1) % QUEUE_MAX;
 
     pthread_cond_signal(&q->nonempty);
@@ -49,7 +63,7 @@ void q_append(Queue* q, char* msg){
 }
 
 //put the current thread to sleep based on if the queue is empty then store head tokenised elements in out arr
-void q_pop(Queue * q, char * out[]){
+void q_pop(Queue * q, char * out[], struct sockaddr_in * sender){
     pthread_mutex_lock(&q->lock);
 
     while (q->head == q->tail){
@@ -61,7 +75,9 @@ void q_pop(Queue * q, char * out[]){
     char * head_data;
     
     get_and_tokenise(q, q->head, out);
+    *sender = q->data[q->head]->from_addr;
     
+    pthread_cond_signal(&q->nonfull);
     pthread_mutex_unlock(&q->lock);
 }
 
