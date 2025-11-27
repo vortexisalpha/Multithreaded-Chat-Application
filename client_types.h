361a821f5249724ae1989dee4c709106d1ba15c4
vortexisalpha <joshuaboshua250505@gmail.com>
2025-11-27 16:16:28 +0000

    add client types seperate file and initilisation commands for setup

---
+++ client_types.h
@@ -0,0 +1,26 @@
+#include "udp.h"
+
+
+//client
+//i dont think we need a port
+typedef struct {
+    char name[NAME_SIZE];
+    bool connected;
+
+    int sd;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in responder_addr;
+
+} client_t;
+
+
+void setup_client(client_t* client){
+    printf("opening socket... \n");
+
+    client->connected = true;
+    client->sd = udp_socket_open(0);;
+
+    printf("setting up port... \n");
+    int rc = set_socket_addr(&client->server_addr, "127.0.0.1", SERVER_PORT);
+    if (rc > 0) printf("Client failed to connect to server");
+}
