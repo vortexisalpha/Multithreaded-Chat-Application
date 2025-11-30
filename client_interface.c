fb83e33f3c07cea9d777e4682dddbef402155775
vortexisalpha <joshuaboshua250505@gmail.com>
2025-11-30 14:13:46 +0000

    integrate mute table with client and check if client is muted before printing message

--- client_interface.c
+++ client_interface.c
@@ -226,6 +226,9 @@ void *chat_display(void *arg){
         int total_messages = *chat_args->message_count;
         hash_node_t **mute_table = chat_args->client->mute_table;
         for (int i = 0; i < total_messages; i++){
+            char *username;
+            extract_username(chat_args->messages[i], username, NAME_SIZE);
+            if(contains(mute_table, username)) continue;
             printf("%s\n", chat_args->messages[i]);
         }
         
