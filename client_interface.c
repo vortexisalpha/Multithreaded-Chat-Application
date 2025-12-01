ffccf3fde7cbd6dc3c375bd97aa7403d84991dd2
vortexisalpha <joshuaboshua250505@gmail.com>
2025-12-01 17:38:25 +0000

    minor bug fixes in client interface and server interface

--- client_interface.c
+++ client_interface.c
@@ -139,7 +139,7 @@ void *chat_display(void *arg){
         int total_messages = *chat_args->message_count;
         hash_node_t **mute_table = chat_args->client->mute_table;
         for (int i = 0; i < total_messages; i++){
-            char *username;
+            char username[NAME_SIZE];
             extract_username(chat_args->messages[i], username, NAME_SIZE);
             if(contains(mute_table, username)) continue;
             printf("%s\n", chat_args->messages[i]);
